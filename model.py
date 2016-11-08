4fbbc54c9d5f485126a7c1c9e73c5cedf6dd2704
NetherNova <martin.ringsquandl@googlemail.com>
2016-11-08 16:02:52 +0100

    add model.py

---
+++ model.py
@@ -0,0 +1,11 @@
+import numpy as np
+import scipy
+import scipy.sparse
+import theano
+import theano.sparse as S
+import theano.tensor as T
+from collections import OrderedDict
+import sys
+import time
+
+
