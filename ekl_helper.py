a5cf50d6031d1d39d0ab3f63896859acc527f0ce
swathi-ssunder <swathi.ssunder@gmail.com>
2018-03-11 12:54:38 +0100

    Link predictions for entities, changes for integration to the Django kg ui

--- ekl_helper.py
+++ ekl_helper.py
@@ -51,18 +51,49 @@ from prep.preprocessing import PreProcessor
 from experiments.experiment_helper import slice_ontology, get_kg_statistics, get_low_dim_embs, get_zero_shot_scenario, \
     cross_parameter_eval, TranslationModels, embs_to_df, Parameters, evaluate_on_test
 
-rnd = np.random.RandomState(42)
+import os
+from django.conf import settings
+from time import time
+from utils import url_parse
 
-if __name__ == '__main__':
+rnd = np.random.RandomState(42)
+status = ""
+
+
+def get_status():
+    return status
+
+
+def init(args):
+    global path_to_kg, path_to_sequence, path_to_uri_map, modeltype, eventlayer, storeembeddings, embedding_size, seq_data_size, \
+        batch_size, learning_rate, lambd, num_epochs, test_proportion, validation_proportion, preprocessor, traffic_data, \
+        base_path, path_to_store_model, amberg_params, exclude_rels, path_to_store_embeddings
+
+    path_to_kg = args.get('path_to_kg')
+    # path_to_sequence = args.get('path_to_sequence')
+    path_to_uri_map = args.get('path_to_uri_map')
+    modeltype = args.get('model_type')
+    # eventlayer = args.get('event_layer')
+    # storeembeddings = args.get('store_embeddings')
+    embedding_size = int(args.get('embedding_size'))
+    # seq_data_size = int(args.get('seq_data_size'))
+    batch_size = int(args.get('batch_size'))
+    learning_rate = float(args.get('learning_rate'))
+    lambd = float(args.get('lambd'))
+    num_epochs = int(args.get('num_epochs'))
+    test_proportion = float(args.get('test_proportion'))
+    validation_proportion = float(args.get('validation_proportion'))
+
     ####### PATH PARAMETERS ########
-    base_path = "./clones/"  # "./traffic_data/"
+    base_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "sim_data/")  # "./traffic_data/"
     path_to_store_model = base_path + "Embeddings/"
     path_to_events = base_path + "Sequences/"
-    path_to_kg = base_path + "Ontology/clones_enhanced.rdf"  # "Ontology/traffic_individuals.xml" #
+    # path_to_kg = base_path + "Ontology/test_2.xml"  # "Ontology/traffic_individuals.xml" #
     path_to_store_sequences = base_path + "Sequences/"
-    path_to_store_embeddings = base_path + "Embeddings/"
+    path_to_store_embeddings = os.path.join(os.path.join(settings.BASE_DIR, "static"),
+                                            "data/")  # path_to_store_embeddings = base_path + "Embeddings/"
     traffic_data = False
-    path_to_sequence = base_path + 'Sequences/sequence.txt'
+    # path_to_sequence = base_path + 'Sequences/sequence_2.txt'
     num_sequences = None
     pre_train = False
     supp_event_embeddings = None  # base_path + "Embeddings/supplied_embeddings.pickle"
@@ -74,19 +105,25 @@ if __name__ == '__main__':
         max_events = 10000
         exclude_rels = [RDFS.comment]
         preprocessor = PreProcessor(path_to_kg)
-        excluded_events = preprocessor.load_unique_msgs_from_txt(base_path + 'unique_msgs.txt', max_events=max_events)
+        excluded_events = preprocessor.load_unique_msgs_from_txt(path_to_uri_map,
+                                                                 max_events=max_events)  # 'unique_msgs.txt'
         excluded_events = [URIRef(('http://www.siemens.com/citypulse#' + e)) for e in excluded_events]
         amberg_params = None
     else:
         exclude_rels = ['http://www.siemens.com/ontology/demonstrator#tagAlias']
         max_events = None
         max_seq = None
-        preprocessor.load_unique_msgs_from_txt(base_path + 'unique_msgs.txt')
+        preprocessor.load_unique_msgs_from_txt(path_to_uri_map)  # 'unique_msgs2.txt'
         # sequence window size in minutes
         window_size = 0.5
         amberg_params = None
         # amberg_params = (path_to_events, max_events)
 
+
+def start_experiment():
+    global entity_embs, reverse_entity_dictionary, relation_embs, reverse_relation_dictionary, num_sequences, g, ent_dict, rel_dict, status
+
+    status = "Loading Knowledge Graph..."
     preprocessor.load_knowledge_graph(format='xml', exclude_rels=exclude_rels, amberg_params=amberg_params)
     vocab_size = preprocessor.get_vocab_size()
     unique_msgs = preprocessor.get_unique_msgs()
@@ -95,15 +132,16 @@ if __name__ == '__main__':
     g = preprocessor.get_kg()
 
     print("Read {0} number of triples".format(len(g)))
+    status = "Knowledge Graph loaded..."
     # get_kg_statistics(g)
 
     zero_shot_triples = []
 
     ######### Model selection ##########
-    model_type = TranslationModels.Trans_E  # TEKE, RESCAL
+    model_type = getattr(TranslationModels, modeltype)  # TranslationModels.Trans_E  # TEKE, RESCAL
     bernoulli = True
-    event_layer = Skipgram
-    store_embeddings = False
+    event_layer = None  # globals()[eventlayer]  # Skipgram
+    store_embeddings = True  # storeembeddings
 
     # pre-train event embeddings
     pre_train = False
@@ -112,17 +150,17 @@ if __name__ == '__main__':
 
     ######### Hyper-Parameters #########
     param_dict = {}
-    param_dict['embedding_size'] = [40]
-    param_dict['seq_data_size'] = [1.0]
-    param_dict['batch_size'] = [32]  # [32, 64, 128]
-    param_dict['learning_rate'] = [0.2]  # [0.5, 0.8, 1.0]
-    param_dict['lambd'] = [0.001]  # regularizer (RESCAL)
+    param_dict['embedding_size'] = [embedding_size]  # [40]
+    param_dict['seq_data_size'] = [1.0]  # [seq_data_size]
+    param_dict['batch_size'] = [batch_size]  # [32]  # [32, 64, 128]
+    param_dict['learning_rate'] = [learning_rate]  # [0.2]  # [0.5, 0.8, 1.0]
+    param_dict['lambd'] = [lambd]  # [0.001]  # regularizer (RESCAL)
     param_dict['alpha'] = [0.5]  # event embedding weighting
     eval_step_size = 1000
-    num_epochs = 100
+    # num_epochs = 100
     num_negative_triples = 2
-    test_proportion = 0.2
-    validation_proportion = 0.1  # 0.1
+    # test_proportion = 0.2
+    # validation_proportion = 0.1  # 0.1
     bernoulli = True
     fnsim = l2_similarity
 
@@ -161,7 +199,7 @@ if __name__ == '__main__':
         bern_probs = bernoulli_probs(g, rel_dict)
 
     # free some memory
-    g = None
+    # g = None  # Note: g is required to exclude existing links from link predictions, hence do not set to None
     model_name = TranslationModels.get_model_name(event_layer, model_type)
     overall_best_performance = np.inf
     best_param_list = []
@@ -208,6 +246,7 @@ if __name__ == '__main__':
             num_sampled = 0
             event_model = None
             pre_train = False
+            num_skips = 0
 
         # Model Selection
         if model_type == TranslationModels.Trans_E:
@@ -242,7 +281,7 @@ if __name__ == '__main__':
             saver = tf.train.Saver(model.variables())
             tf.global_variables_initializer().run()
             print('Initialized graph')
-
+            status = "Initializing knowledge graph..."
             average_loss = 0
             mean_rank_list = []
             hits_10_list = []
@@ -318,6 +357,7 @@ if __name__ == '__main__':
                     if overall_best_performance > mean_rank:
                         overall_best_performance = mean_rank
                         print("Saving overall best model with MeanRank: {0} and hits {1}".format(mean_rank, hits_10))
+                        status = "Saving overall best model in epoch {0}...".format(b)
                         save_path_global = saver.save(session, path_to_store_model + 'tf_model')
                         best_param_list = param_list
 
@@ -326,16 +366,13 @@ if __name__ == '__main__':
 
             # save embeddings to disk
             if store_embeddings:
-                for i in range(len(entity_embs)):
-                    if i % 50 == 0:
-                        df_embs = get_low_dim_embs(entity_embs[i], reverse_entity_dictionary)
-                        df_embs.to_csv(path_to_store_embeddings + "entity_embeddings_low" + str(i) + ".csv", sep=',',
-                                       encoding='utf-8')
+                # Generate only one the first embedding initially..
+                # Subsequent embeddings are generated dynamically, when invoked from the Front-end
+                for i in range(1):  # range(len(entity_embs))
+                    run_iteration(i)
 
-                        df_r_embs = get_low_dim_embs(relation_embs[i], reverse_relation_dictionary)
-                        df_r_embs.to_csv(path_to_store_embeddings + "relation_embeddings" + str(i) + ".csv", sep=',',
-                                         encoding='utf-8')
-
+                print("Saving best model embeddings...")
+                status = "Saving best model embeddings..."
                 # TODO: only of best model (not last)
                 df_embs = embs_to_df(entity_embs[len(entity_embs) - 1], reverse_entity_dictionary)
                 df_embs.to_csv(path_to_store_embeddings + "entity_embeddings" + '_last_cleaned' + ".csv", sep=',',
@@ -361,3 +398,72 @@ if __name__ == '__main__':
                  batch_size_sg, relation_results[rel]['MeanRank'], relation_results[rel]['MRR'],
                  relation_results[rel]['Hits@10'], relation_results[rel]['Hits@3'], relation_results[rel]['Hits@1']]
             )
+    status = "Done."
+
+
+def generate_link_predictions(i):
+    global status
+    # Select Top-k links for each src_entity
+    k = 3
+
+    start = time()
+    print("Generating link predictions...", entity_embs[i].shape, relation_embs[i].shape)
+    status = "Generating link predictions..."
+    existing_links = [(ent_dict[url_parse(s)], rel_dict[url_parse(p)], ent_dict[url_parse(o)]) for (s, p, o) in
+                               g.triples((None, None, None))]
+
+    # Compute all possible combinations for links between entities based on the relations -> (n_e * n_r * n_e-1) links
+    temp = np.sqrt(np.square(entity_embs[i][:, None, None] + relation_embs[i][None, :, None] - entity_embs[i][None, None, :]).sum(axis=3, keepdims=True))
+
+    # Exclude already existing links/known links
+    for l in existing_links:
+        temp[l] = 0
+
+    # # Note: Self-relations/Loops are not explicitly excluded. It is later verified that these relations do not have
+    # # high probability of being formed-hence are automatically excluded from the recommendations
+
+    grid = np.indices((len(ent_dict), len(rel_dict), len(ent_dict)))
+    s, p, o = grid[0].reshape(-1), grid[1].reshape(-1), grid[2].reshape(-1)
+    ids = list(zip(s, p, o))
+
+    s_wise = temp.reshape(len(ent_dict), -1)
+    sort_idx = np.argsort(-s_wise, axis=1)
+
+    topk_predictions = [['_'.join(str(s) for s in ids[i * len(ent_dict) * len(rel_dict) + sort_idx[i][j]]), s_wise[i, sort_idx[i][j]]] for i in range(len(s_wise)) for j in range(k)]
+    topk_predictions = np.hstack((topk_predictions, np.tile(np.array([[i] for i in range(k)]), (len(ent_dict), 1))))
+
+    np.savetxt(path_to_store_embeddings + "topk_predictions_" + str(i) + ".csv", topk_predictions,
+               header="subject_predicate_object,score,rank", delimiter=',', fmt='%s,%s,%s',
+               comments='')
+
+    print("Link predictions saved in", time() - start, "seconds")
+    status = "Done."
+
+
+def get_entity_class(entity_uri):
+    if '#' in entity_uri:
+        entity_class = entity_uri.split('#')[1].strip('- ')
+    else:
+        entity_class = entity_uri
+
+    return entity_class.replace('\d+', '')
+
+
+def run_iteration(i):
+    global status
+
+    status = "Computing low dimensional embeddings..."
+    # Store the entity & relation embeddings to respective files
+    df_embs = get_low_dim_embs(entity_embs[i], reverse_entity_dictionary)
+
+    # Compute the class of the entities based on the uri
+    df_embs['class'] = df_embs['uri'].apply(lambda x: get_entity_class(x))
+    df_embs['class'] = df_embs['class'].str.replace('\d+', '')
+
+    status = "Saving embeddings..."
+    df_embs.to_csv(path_to_store_embeddings + "entity_embeddings_low" + str(i) + ".csv", sep=',', encoding='utf-8')
+
+    df_r_embs = get_low_dim_embs(relation_embs[i], reverse_relation_dictionary)
+    df_r_embs.to_csv(path_to_store_embeddings + "relation_embeddings_low" + str(i) + ".csv", sep=',', encoding='utf-8')
+
+    generate_link_predictions(i)
